e7e6e47070bb3fdb1a15acf4ae6900269e19930c
Chris Mahlke <cmahlke@gmail.com>
2026-05-05 08:41:10 -0700

    Initial Commit

---
+++ aiTests/aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  aiTests.swift
+//  aiTests
+//
+//  Created by Chris Mahlke on 5/5/26.
+//
+
+import Testing
+@testable import ai
+
+struct aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
